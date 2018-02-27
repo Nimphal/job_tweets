af8dd514ef4c7f0e65a453d7c09b47f65f21fe43
Nevelina Aleksandrova <nevelina@cottagelabs.com>
2018-02-27 09:41:13 +0000

    [WIP] talks to twitter, needs tests

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,22 @@
 class TweetsController < ActionController::Base	
 	layout 'application'
 
+	def initialize
+		@client = Twitter::REST::Client.new do |config|
+		  config.consumer_key        = ENV["CONSUMER_KEY"]
+		  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+		  config.access_token        = ENV["ACCESS_TOKEN"]
+		  config.access_token_secret = ENV["ACCESS_SECRET"]
+		end
+	end
+
 	def search
 		search_term = params[:q]
+		@results = []
 		if search_term
-			@results=[{title: search_term}, {title: "tweet2"}]
-		else
-			@results = []
+			@client.search("#{search_term}", result_type: "recent").take(3).collect do |tweet|
+	  			@results << tweet
+			end
 		end
 		render "tweets"
 	end
